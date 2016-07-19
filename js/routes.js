222eb503181b19e50493d4c3cfa2f5ef50d8c86d
Dominik George <nik@naturalnet.de>
2016-07-19 23:54:03 +0200

    Comment routes.

--- js/routes.js
+++ js/routes.js
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// All route definitions for app views
 app.config(function($routeProvider) {
     $routeProvider.when('/', {
         redirectTo: '/map'
