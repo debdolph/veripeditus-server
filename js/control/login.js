26a6ac72c41e8c1f856669c5f783e1c8e7e34835
Dominik George <nik@naturalnet.de>
2016-07-23 23:31:28 +0200

    Complete login modal.

--- js/control/login.js
+++ js/control/login.js
@@ -16,10 +16,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-app.controller('ViewLoginController', function($scope, $window, APIBasicAuth) {
+app.controller('ViewLoginController', function($scope, $uibModalInstance, APIBasicAuth) {
     $scope.login = function() {
         APIBasicAuth.login($scope.username, $scope.password, $scope.remember);
-        // FIXME: Only on success
-        $window.history.back();
+        // FIXME only on success
+        $uibModalInstance.close();
     };
 });
