8e219b2d4cd915a8a4e7abd7d97b478c0c7d06c6
Dominik George <nik@naturalnet.de>
2016-07-31 00:33:02 +0200

    Added code for objects in camera view (progressing #15).
    Still very incomplete and buggy.

--- www/js/control/cam.js
+++ www/js/control/cam.js
@@ -17,6 +17,8 @@
  */
 
 app.controller('ViewCamController', function($log, $document, $scope, GameDataService, DeviceService) {
+    const MAX_DISTANCE = 100;
+
     // Find video view
     $scope.cam = $document.find('video')[0];
 
@@ -38,18 +40,70 @@ app.controller('ViewCamController', function($log, $document, $scope, GameDataSe
     // Start camera
     DeviceService.startCamera();
 
+    // Utility functions for generating player images
+    $scope.getARStyle = function(player) {
+        // Target object
+        var style = {}
+
+        // Get perspective value
+        // FIXME get dynamically
+        var perspective = 800;
+
+        // Center image first
+        var width = document.getElementById("arplayer-" + player.id).width;
+        style.left = ((screen.width - width) / 2) + "px";
+
+        // Get own LatLng
+        var own_latlng = L.latLng(DeviceService.position.coords.latitude, DeviceService.position.coords.longitude);
+        // Get player LatLng
+        var player_latlng = L.latLng(player.latitude, player.longitude);
+
+        // Get distance and bearing
+        var distance = own_latlng.distanceTo(player_latlng);
+        var bearing = own_latlng.bearingTo(player_latlng);
+        // Determine difference of bearing and device orientation
+        var bearing_diff = DeviceService.orientation.alpha - bearing;
+
+        // Calculate offsets in 3D space in relation to camera
+        // FIXME take distance into account
+        var angle = (((-bearing_diff) % 360) / 360) * (2*Math.PI);
+        var px = Math.sin(angle) * perspective;
+        var py = 0;
+        var pz = perspective - Math.cos(angle) * perspective;
+
+        // Generate transform functions
+        var rotation = "rotateY(" + (bearing_diff) + "deg)";
+        var offset = "translate3d(" + px + "px, " + py + "px, " + pz + "px)";
+
+        // Generate CSS transform attribute
+        style.transform = rotation + " " + offset;
+
+        return style;
+    };
+    $scope.getPlayerAvatar = function(player) {
+        return '/api/data/avatar_' + player.avatar + '.svg';
+    };
+
     // Subscribe to data updates for surrounding players
     $scope.$on('GameData.updated.players', function(event, players) {
-        // FIXME add code
+        // Map players into scope
+        $scope.players = angular.copy(players);
     });
 
     // Subcribe to geolocation updates
     $scope.$on('Geolocation.changed', function(event, position) {
         // Calculate view bounds
         // FIXME come up with something smarter
-        var bounds = [[position.latitude - 0.001, position.longitude - 0.001], [position.latitude + 0.001, position.longitude + 0.001]];
+        var bounds = [[position.coords.latitude - 0.001, position.coords.longitude - 0.001], [position.coords.latitude + 0.001, position.coords.longitude + 0.001]];
 
         // Update bounds in GameDataService
         GameDataService.setBounds(bounds[0], bounds[1]);
+    });
+
+    // Subcribe to orientation updates
+    $scope.$on('Orientation.changed', function(event) {
+        // Force update of player images
+        // FIXME come up with something better
+        $scope.players = angular.copy($scope.players);
     });
 });
