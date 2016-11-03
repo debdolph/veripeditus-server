ece52ca9e21fa03406e9a64cb3c3c9cdb642bb1e
Dominik George <nik@naturalnet.de>
2016-11-03 21:51:37 +0100

    Fix missing import.

--- veripeditus/framework/util.py
+++ veripeditus/framework/util.py
@@ -18,6 +18,7 @@ Utility functions for framework components
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
 import os
 import random
 
