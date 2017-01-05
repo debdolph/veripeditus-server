bf73592e7fdff0e6b9a605314554289d6e406560
Dominik George <nik@naturalnet.de>
2017-01-05 17:56:21 +0100

    Remove debug alert.

--- www/src/device.js
+++ www/src/device.js
@@ -44,8 +44,6 @@ DeviceService = function() {
         this.position.coords = newpos.coords;
         this.position.timestamp = newpos.timestamp;
 
-        alert(newpos.coords.latitude);
-
         // Call onGeolocationChanged on all views
         $.each(Veripeditus.views, function (id, view) {
             if (view.onGeolocationChanged) {
